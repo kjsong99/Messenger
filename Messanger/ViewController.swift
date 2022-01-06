20d0c30157ab8351f22ab2c2523098a2b0161a35
kjsong99 <mine7118@kpu.ac.kr>
2022-01-06 11:33:47 +0900

    Getting Started

---
+++ Messanger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messanger
+//
+//  Created by 송경진 on 2022/01/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
